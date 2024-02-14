e5031288d1c94fbe351cc292e210f3762d3beae3
James Wu <jameswu@Jamess-MacBook-Air-2.local>
2024-02-13 18:59:47 -0600

    API endpoints for user login and creation

--- backend/README.md
+++ backend/README.md
@@ -10,3 +10,27 @@
 - Run command "npm run dev"
 
 https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases-typescript-postgresql
+
+## Using the REST API Endpoints
+
+You can access the REST API of the server using the following endpoints:
+
+### `POST`
+
+- `/user/create`: Create a new user
+  - Body:
+    - `username: String` (required): The user's username
+    - `email: String` (required): The user's email, must be unique
+    - `password: String` (required): The user's password
+  - Response:
+    - `201`: Account created successfully
+    - `400`: Invalid request made
+    - `409`: Duplicate email used
+- `/user/create`: Create a new user
+  - Body:
+    - `email: String` (required): The user's email
+    - `password: String` (required): The user's password
+  - Response:
+    - `200`: Logged in successfully
+    - `400`: Invalid request made
+    - `401`: Invalid email or password given
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import { Prisma, PrismaClient } from "@prisma/client";
-import { log } from "console";
 import express from "express";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 const app = express();
@@ -12,3 +12,67 @@ const server = app.listen(3000, () =>
 🚀 Server ready at: http://localhost:3000
 ⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
 );
+
+app.post(`/user/create`, async (req, res) => {
+  const { username, email, password } = req.body;
+  const password_hashed = await hashPassword(password);
+  try {
+    const result = await prisma.user.create({
+      data: {
+        email,
+        username,
+        password_hashed,
+        last_login: new Date().toISOString(),
+      },
+    });
+    res.sendStatus(201);
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      console.error(e);
+      res.sendStatus(409);
+    } else {
+      res.sendStatus(400);
+    }
+  }
+});
+
+app.post(`/user/login`, async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const result = await prisma.user.findUnique({
+      where: { email: email },
+    });
+    if (result == null) {
+      res.sendStatus(401);
+    } else {
+      if (await comparePasswords(password, result.password_hashed)) {
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(401);
+      }
+    }
+  } catch (e) {
+    res.sendStatus(400);
+  }
+});
+
+async function hashPassword(password: string): Promise<string> {
+  const salt = await bcrypt.genSalt(10); // 10 is the number of salt rounds
+
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  return hashedPassword;
+}
+
+async function comparePasswords(
+  plainPassword: string,
+  hashedPassword: string
+): Promise<boolean> {
+  try {
+    const match = await bcrypt.compare(plainPassword, hashedPassword);
+    return match;
+  } catch (error) {
+    console.error("Error comparing passwords:", error);
+    throw new Error("Error comparing passwords");
+  }
+}
