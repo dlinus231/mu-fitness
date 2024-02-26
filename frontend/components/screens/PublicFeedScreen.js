390ab4882c547ecf6c6518a20d2cb7183d89b9ed
Ben Mueller <benjm2002@gmail.com>
2024-02-26 14:21:03 -0600

    added search and dms to public feed

--- frontend/components/screens/PublicFeedScreen.js
+++ frontend/components/screens/PublicFeedScreen.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, View } from '@gluestack-ui/themed';
 
+import TopBarMenu from '../TopBarMenu';
+
+import DirectMessagesScreen from './DirectMessagesScreen';
+import SearchScreen from './SearchScreen';
+
 const PublicFeedScreen = () => {
+  // enum, can have values 'publicFeed', 'dms', 'search
+  const [ curPage, setCurPage ] = useState('publicFeed'); 
+
+  const handleSwitchPage = (page) => {
+    setCurPage(page);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>This is the public feed screen</Text>
-    </View>
+    <>
+      { curPage == 'dms' && <DirectMessagesScreen 
+        onSwitchPage={handleSwitchPage} 
+        rootPage='publicFeed'
+      /> }
+      { curPage == 'search' && <SearchScreen 
+        onSwitchPage={handleSwitchPage} 
+        rootPage='publicFeed'
+      />}
+      { curPage == 'publicFeed' && (
+        <>
+          <TopBarMenu onSwitchPage={handleSwitchPage}/>
+          <View style={styles.container}>
+            <Text>This is the public feed screen</Text>
+          </View>
+        </>
+      )}
+    </>
   );
 };
 
