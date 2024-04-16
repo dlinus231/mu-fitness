e8da0e276aa4dbeeaf8d8bb93c9d317b2b667b87
dlinus231 <71240716+dlinus231@users.noreply.github.com>
2024-04-16 17:21:05 -0500

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const server = app.listen(3000, () =>
+const server = app.listen(process.env.PORT 3000, () =>
   console.log(`
 🚀 Server ready at: http://localhost:3000
 ⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
